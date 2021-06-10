f7f9278a8eba645d2540167df9837d0ea4c62c2c
Johannes Winter <jrandom@speed.at>
2021-06-10 22:34:26 +0200

    rt: extract common DMA processing functionality

--- src/movme/build.sh
+++ src/movme/build.sh
@@ -1,4 +1,4 @@
-../../external/movfuscator/build/movcc -S -O3 -Wf--no-mov-flow -Wf--no-mov-extern -Wf--no-mov-loop movme.c || exit 1
-python3 movme-dma.py --input=movme.s --output=movme_dma.s || exit 1
-arm-none-eabi-gcc -o movme-host.elf -Wall -O2 -g -specs=rdimon.specs -mcpu=arm926ej-s -marm movme-host.c movme_dma.s movcc-datatables.s || exit 1
-arm-none-eabi-gcc -o movme-emu.elf  -Wall -O2 -g -specs=rdimon.specs -mcpu=arm926ej-s -marm movme-emu.c  movme_dma.s movcc-datatables.s || exit 1
+../../external/movfuscator/build/movcc -o moveme.S -S -O3 -Wf--no-mov-flow -Wf--no-mov-extern -Wf--no-mov-loop movme.c || exit 1
+python3 movme-dma.py --input=movme.S --output=movme_dma.S || exit 1
+arm-none-eabi-gcc -o movme-host.elf -I.. -Wall -O2 -g -specs=rdimon.specs -mcpu=arm926ej-s -marm movme-host.c movme_dma.S movcc-datatables.S || exit 1
+arm-none-eabi-gcc -o movme-emu.elf  -I.. -Wall -O2 -g -specs=rdimon.specs -mcpu=arm926ej-s -marm movme-emu.c  movme_dma.S movcc-datatables.S || exit 1
