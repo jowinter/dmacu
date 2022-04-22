5a75ce8e80f04eecf218792b74a9034ca3885a8a
Johannes Winter <jrandom@speed.at>
2022-04-22 02:14:17 +0200

    cleanup: purge legacy (asm) and incomplete (movfuscator emulation) content - it can be revived at a later point in time

--- src/movme/build.sh
+++
@@ -1,4 +0,0 @@
-../../external/movfuscator/build/movcc -o moveme.S -S -O3 -Wf--no-mov-flow -Wf--no-mov-extern -Wf--no-mov-loop movme.c || exit 1
-python3 movme-dma.py --input=movme.S --output=movme_dma.S || exit 1
-arm-none-eabi-gcc -o movme-host.elf -I.. -Wall -O2 -g -specs=rdimon.specs -mcpu=arm926ej-s -marm movme-host.c movme_dma.S movcc-datatables.S || exit 1
-arm-none-eabi-gcc -o movme-emu.elf  -I.. -Wall -O2 -g -specs=rdimon.specs -mcpu=arm926ej-s -marm movme-emu.c  movme_dma.S movcc-datatables.S || exit 1
